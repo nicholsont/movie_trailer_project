cc1da18ed665389faf9daf5a16fe830285db4d53
Tijan Nicholson <nikorusontijan@nikorusijansMBP.attlocal.net>
2017-11-14 09:36:41 -0800

    Formatted to PEP8 standard

--- Script/media.py
+++ Script/media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 # Creates the movie class
 class Movie():
     # Initializes the movie class
@@ -9,6 +10,7 @@ class Movie():
         self.rating = rating
         self.poster_image_url = poster_image_url
         self.trailer_url = trailer_url
+
     # Opens the movie trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
